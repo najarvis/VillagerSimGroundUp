6ef2765cf00cfafb6feaf5544976187f16db9628
Nick <najarvis2016@gmail.com>
2022-08-28 19:45:16 -0400

    Basic rendering of grid of tiles

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 
-SCREEN_SIZE = WIDTH, HEIGHT = (640, 480)
+from tile import Tile
+
+SCREEN_SIZE = WIDTH, HEIGHT = (640, 640)
 
 WORLD_SIZE = (20, 20)
 TILE_SIZE = (32, 32)
+
+tiles = [[Tile(pygame.Vector2(x * TILE_SIZE[0], y * TILE_SIZE[1]), TILE_SIZE) for x in range(WORLD_SIZE[0])] for y in range(WORLD_SIZE[1])]
 
 def run():
     screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -18,7 +22,10 @@ def run():
                 if event.key == pygame.K_ESCAPE:
                     done = True
 
-
+        for tile_row in tiles:
+            for tile in tile_row:
+                tile.draw(screen)
+            
         pygame.display.update()
 
 if __name__ == "__main__":
