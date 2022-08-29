658b1eb22c9cfa406b3bbdf9179b9878f56d5af8
Nick <najarvis2016@gmail.com>
2022-08-28 20:02:33 -0400

    Fix crash

--- tile.py
+++ tile.py
@@ -2,9 +2,13 @@ import pygame
 
 class Tile:
 
-    def __init__(self, coord: pygame.Vector2, size: tuple[int, int]):
+    def __init__(self, coord: pygame.Vector2, size: int | tuple[int, int]):
         self.position = coord
-        self.size = self.width, self.height = size
+        
+        if type(size) is int:
+            self.size = self.width, self.height = (size, size)
+        else:
+            self.size = self.width, self.height = size
 
     def draw(self, surface: pygame.Surface) -> None:
         pygame.draw.rect(surface, (255, 255, 255), (self.position.x, self.position.y, self.width, self.height), 2)
