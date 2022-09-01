2eb683368c780995ad6a9585fffd4e59b5a7edfc
Nick Jarvis <najarvis2016@gmail.com>
2022-09-01 00:12:08 -0400

    Clean up

--- camera.py
+++ camera.py
@@ -1,18 +1,23 @@
 import pygame
 
 class Camera:
+    """Used to keep track of what should be on the screen. 
+    Represents the virtual camera.
+    """
 
-    def __init__(self, pos):
+    def __init__(self, pos: pygame.Vector2):
         self._scale = 1.0
         self.position = pos
 
     def world_to_screen(self, coord):
-        return coord - self._scale * self.position
+        # something like (coord - self.position) * self._scale
+        NotImplemented
 
     def get_scale(self) -> float:
         return self._scale
 
     def set_scale(self, val) -> None:
+        # clamp the values
         self._scale = max(0.1, min(val, 10))
 
     scale = property(get_scale, set_scale)
